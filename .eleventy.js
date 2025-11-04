c019f81e3e28a92ba9a67212ff68ae7b6af75572
angel33la <angel33la@outlook.com>
2025-11-04 14:04:13 -0500

    Adding Eleventy to my repo

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+export default function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("scripts");
+
+    return {
+        dir: {
+            input: ".",
+            includes: "_includes",
+            layouts: "_includes/layouts",
+            data: "_data",
+            output: "_site",
+        },
+        passthroughFileCopy: true,
+    };
+};
